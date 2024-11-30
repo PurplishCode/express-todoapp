9ab515321ba4d8f876aa12418455c422fb797456
Droplet <darellpetualang@gmail.com>
2024-12-01 00:11:21 +0700

    I just renamed the routing signup!!

--- routes/user_router.js
+++ routes/user_router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userController = require('../controllers/user_controller');
 
 // Router POST
-router.post('/register', userController.register);
+router.post('/signup', userController.register);
 router.post('/signin', userController.signin);
 
 module.exports = router;
